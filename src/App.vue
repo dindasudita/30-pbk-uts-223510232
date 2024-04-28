a408a91cfe084a545afa74d5113ef629cf8f0564
dindasudita <dindasudita2016@gmail.com>
2024-04-28 19:30:17 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>   
+  <ToDo />
+  </div>
+</template>
+
+
+<script setup>
+import ToDo from './components/ToDo.vue';
+</script>
+
+<style>
+body{
+  background-color: #fad0c9;
+}
+</style>
